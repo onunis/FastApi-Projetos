cfd786282ca34cfddd83bfedb680fe4cac716e0a
Guilherme <mitaogui@gmail.com>
2026-08-05 21:47:51 -0300

    feat: criando novo projeto books2

---
+++ books2.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Book:
+    id: int  #Type Hints
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+BOOKS = []
+
+@app.get("/books")
+async def real_all_books():
+    return BOOKS
