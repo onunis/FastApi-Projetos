411913b022b1da97cac9ad0657d89c86f0c17176
Guilherme <mitaogui@gmail.com>
2026-08-05 22:05:26 -0300

    feat: criando lista com os objetos da classe book

--- books2.py
+++ books2.py
@@ -17,7 +17,14 @@ class Book:
         self.rating = rating
 
 
-BOOKS = []
+BOOKS = [
+    Book(1, "Tudo e Rio", "Carla Madeira", "Um livro muito bom", 5),
+    Book(2, "jujutsu kaisen", "Mangaka random", "Um livro muito bom", 5),
+    Book(3, "A Natureza da Mordida", "Carla Madeira", "Um livro bom", 4),
+    Book(4, "CS Pro", "Coding with Ruby", "Um livro muito bom", 3),
+    Book(5, "HP2", "Author 1", "Um livro muito bom", 2),
+    Book(6, "HP1", "Author 2", "Um livro muito bom", 1)
+]
 
 @app.get("/books")
 async def real_all_books():
