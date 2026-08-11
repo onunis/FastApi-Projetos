4ae82cce83d08e15995d67d8b9dc352eb11f1eac
Guilherme <mitaogui@gmail.com>
2026-08-10 22:23:01 -0300

    feat: adicionando end-point pra buscar livros por rating

--- books2.py
+++ books2.py
@@ -47,6 +47,16 @@ async def book_id(book_id: int):
             return book
         
 
+@app.get("/books/")
+async def books_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
+
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
