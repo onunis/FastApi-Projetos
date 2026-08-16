1930d17134f51cf0662998823af803c27292f29e
Guilherme <mitaogui@gmail.com>
2026-08-15 21:50:39 -0300

    feat: criando dependency injection finalmente

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+dp_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: dp_dependency):
+    return db.query(Todos).all()
+
