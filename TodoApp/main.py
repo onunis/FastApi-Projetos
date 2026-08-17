fdb92aa35e302ea602bc296a0d61816030e399ca
Guilherme <mitaogui@gmail.com>
2026-08-16 23:00:25 -0300

    feat: adicionando endpoint put pra inserir informacoes dentro do nosso banco de dados

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,6 @@
+from pydantic import Field
 from typing import Annotated
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 from starlette import status
@@ -20,6 +22,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -31,4 +41,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Id not found")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model) 
+    db.commit()
 
