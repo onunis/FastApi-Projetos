8c91926a2866698ad6c0a20ce60e226a3c1e6f05
Guilherme <mitaogui@gmail.com>
2026-08-19 23:56:14 -0300

    feat: adicionando import do auth no main.py pra rodar todos os endpoints juntos sem ter quer rodar servidor separado.

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,14 +4,17 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 from starlette import status
-
 import models
 from models import Todos
 from database import engine, SessionLocal
+from routers import auth
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
 
 def get_db():
     db = SessionLocal()
