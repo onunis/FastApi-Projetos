0a7dc33e996d90deb341d25421fdff4c942ce0d1
Guilherme <mitaogui@gmail.com>
2026-08-22 16:47:41 -0300

    feat: adicionando owner_id na tabela Todos pra funcionar como chave estrangeira do Users_id

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,7 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
 class Users(Base):
     __tablename__ = 'users'
 
@@ -20,4 +22,5 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
