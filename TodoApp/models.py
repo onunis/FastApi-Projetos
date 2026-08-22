c0ff1abe28e23e0ed5066497d727eb21f0ad1a39
Guilherme <mitaogui@gmail.com>
2026-08-22 16:46:29 -0300

    feat: criando tabela Users no nosso arquivo models

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,6 +1,16 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
+class Users(Base):
+    __tablename__ = 'users'
 
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -10,3 +20,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    
