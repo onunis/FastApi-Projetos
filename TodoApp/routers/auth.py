757394cb11e636f527da6a4c40823422a2983522
Guilherme <mitaogui@gmail.com>
2026-08-26 23:54:22 -0300

    fix: atualizando a rota do nosso token url ja que agora a rota correta e auth/token

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -20,7 +20,7 @@ ALGORITHM = 'HS256'
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(TokenUrl="token")
+oauth2_bearer = OAuth2PasswordBearer(TokenUrl="auth/token")
 
 
 
