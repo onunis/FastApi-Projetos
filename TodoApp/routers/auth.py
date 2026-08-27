4a13a5f51d011cb9de334d61243f020ce553eab7
Guilherme <mitaogui@gmail.com>
2026-08-26 23:57:12 -0300

    fix: atualizando a dependencia tokenurl que estava errada

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -20,7 +20,7 @@ ALGORITHM = 'HS256'
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(TokenUrl="auth/token")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 
