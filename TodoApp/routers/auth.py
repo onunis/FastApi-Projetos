34f72c98334b40b77ed3d2e159b45fb8edf12e2b
Guilherme <mitaogui@gmail.com>
2026-08-26 23:53:38 -0300

    fix: tirando /auth do post pra criar usuario, agora utilizando o prefix=auth

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -79,7 +79,7 @@ class Token(BaseModel):
     token_type: str
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
 
