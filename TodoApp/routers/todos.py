d70e4495f5cea52d57f1efbb3617fcd427ec3036
Guilherme <mitaogui@gmail.com>
2026-08-27 22:45:26 -0300

    feat: inserindo HTTPException com error 401 no endpoint create_todo pra caso User seja None

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -44,6 +44,9 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        return HTTPException(status_code=401, detail="Authentication Failed")
+
     todo_model = Todos(**todo_request.model_dump())
     db.add(todo_model) 
     db.commit()
