af8f804429803979eb1fc6858a9861d8a8d4bf3f
Guilherme Nunes <127986811+onunis@users.noreply.github.com>
2026-08-29 18:25:53 +0000

    feat: criando endpoitn read all todos pros admins, so eles podem ver Todos os todo´s agora

--- TodoApp/routers/admin.py
+++ TodoApp/routers/admin.py
@@ -22,3 +22,10 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
+@router.get("/todo", status_code=status.HTTP_200_OK)
+async def read_all_todos(user: user_dependency, db: db_dependency):
+    if user is None or user.get("user_role") =! "admin":
+        return HTTPException(status_code=401, detail="Authentication Failed")
+    return db.query(Todos).all()
+
