ef09f44f6dd3a0d075528704dc2c8c3b34b6da69
Guilherme Nunes <127986811+onunis@users.noreply.github.com>
2026-08-29 18:34:53 +0000

    feat: importando o router admin pro main e corrigindo erro de condicao no endpoint read all todos no admin

--- TodoApp/routers/admin.py
+++ TodoApp/routers/admin.py
@@ -25,7 +25,7 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.get("/todo", status_code=status.HTTP_200_OK)
 async def read_all_todos(user: user_dependency, db: db_dependency):
-    if user is None or user.get("user_role") =! "admin":
+    if user is None or user.get("user_role") != "admin":
         return HTTPException(status_code=401, detail="Authentication Failed")
     return db.query(Todos).all()
 
