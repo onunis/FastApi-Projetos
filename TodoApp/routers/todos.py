dd13aa11945a05cedbb4c7d80005a164d0cf305d
Guilherme <mitaogui@gmail.com>
2026-08-29 00:10:17 -0300

    feat: adicionando HTTPException 401 no endpoint de update

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -57,7 +57,11 @@ async def create_todo(user: user_dependency, db: db_dependency, todo_request: To
 
 
 @router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_date(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+async def update_date(user: user_dependency, db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    if user is None:
+        return HTTPException(status_code=401, detail="Authentication Failed")
+
+
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail="Todo not found")
