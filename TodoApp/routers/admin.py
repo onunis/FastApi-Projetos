3bc2aefa25bbefbb4546c9ae73962686878f412d
Guilherme Nunes <127986811+onunis@users.noreply.github.com>
2026-08-29 18:18:08 +0000

    feat: criando novo router - admin.py pras rotas so disponiveis a adm. Trazendo imports e depedencias dos outros Routers

---
+++ TodoApp/routers/admin.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, Path
+from starlette import status
+from models import Todos
+from database import SessionLocal
+from .auth import get_current_user
+
+router = APIRouter(
+    prefix='/admin',
+    tags=['admin']
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
