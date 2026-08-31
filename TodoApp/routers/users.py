cf0e3f4b37dac12940d5a59ee101c80b01fb947f
Guilherme Nunes <127986811+onunis@users.noreply.github.com>
2026-08-31 18:20:11 +0000

    feat: criando nova rota users, endpoint pra retornar todas as informações do usuario com httpexception em caso de erros e importando nova rota pra main.

---
+++ TodoApp/routers/users.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, Path
+from starlette import status
+from models import Todos, Users
+from database import SessionLocal
+from .auth import get_current_user
+
+router = APIRouter(
+    prefix='/users',
+    tags=['users']
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_user_info(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+    if user_model is not None:
+        return user_model
+    raise HTTPException(status_code=404, detail="User not Found")
