d49dede4362106192a1bc31cb080dceee1ff954f
Guilherme <mitaogui@gmail.com>
2026-08-31 22:44:23 -0300

    feat: criando endpoint change_password pra mudar a senha do user. Usando bcrypt_context pra bater o hash das senhas e criando um novo

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -38,4 +38,15 @@ async def get_user_info(user: user_dependency, db: db_dependency):
     return db.query(Users).filter(Users.id == user.get("id")).first()
 
 
-    
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, db: db_dependency, user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
