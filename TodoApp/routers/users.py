72859b1240b6718017924c6b90b97765e9972882
Guilherme <mitaogui@gmail.com>
2026-08-31 23:00:55 -0300

    fix: tirando alguns imports que nao precisam de Users.py

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -1,9 +1,9 @@
 from typing import Annotated
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
-from models import Todos, Users
+from models import Users
 from database import SessionLocal
 from .auth import get_current_user
 from passlib.context import CryptContext
