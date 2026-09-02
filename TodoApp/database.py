0378b6f73528f5eef7e95d786e4b208ec68466c0
Guilherme <mitaogui@gmail.com>
2026-09-01 21:57:33 -0300

    fix: tirando connect_args que era peculiaridade do sqlite

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:gui123@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
