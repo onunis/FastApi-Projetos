7a9e400e675d65c45ada748521c08bc006ff6bd6
Guilherme <mitaogui@gmail.com>
2026-09-02 22:59:39 -0300

    refactor: move credenciais de banco para variavel de ambiente

--- TodoApp/alembic/env.py
+++ TodoApp/alembic/env.py
@@ -1,10 +1,15 @@
 from logging.config import fileConfig
-
+import os
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 import models
+
+load_dotenv()
+
+config.set_main_option("sqlalchemy.url", os.getenv("SQLALCHEMY_URL"))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
