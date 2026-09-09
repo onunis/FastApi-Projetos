9a36b5778438432dcd7092b693b2c7021ba9b676
Guilherme <mitaogui@gmail.com>
2026-09-08 23:40:02 -0300

    feat: importando pytest

--- TodoApp/test/test_example.py
+++ TodoApp/test/test_example.py
@@ -1,11 +1,4 @@
-def test_equal():
-    assert 3 == 3
+import pytest
 
-def test_not_equal_fails():
-    assert 3 == 2
 
-def test_not_equal():
-    assert 3 != 1
 
-def test_equal_fails():
-    assert 3 != 3
