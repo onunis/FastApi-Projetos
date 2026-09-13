06768f9ea7124530fb0b0bd5db31e08b5830e374
Guilherme <mitaogui@gmail.com>
2026-09-12 23:57:01 -0300

    feat: adiciondo engine no banco de teste

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,1 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},
+                       poolclass= StaticPool,)
+
