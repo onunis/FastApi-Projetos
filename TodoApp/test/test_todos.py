93df5ff7faaab186d5216dacd42d269adc09afd5
Guilherme <mitaogui@gmail.com>
2026-09-13 00:15:23 -0300

    feat: criando funcao pra imitar dependencia do db

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -13,3 +13,10 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 
 Base.metadata.create_all(bind=engine)
 
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
