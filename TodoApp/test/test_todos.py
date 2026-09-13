fd04c13304fc5338fbdcb39aa9e8c37bbc9a6958
Guilherme <mitaogui@gmail.com>
2026-09-13 00:09:51 -0300

    criando conexao com banco de teste

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from database import Base
+
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},
                        poolclass= StaticPool,)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
 
