60a3483fa2a38e53ba461c91ab44fe3822a8d48b
Guilherme <mitaogui@gmail.com>
2026-09-15 21:54:54 -0300

    fix: corrigindo imports com relative path

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm import sessionmaker
-from ..database import Base
-from ..main import app
-from ..routers.todos import get_db, get_current_user
+from database import Base
+from main import app
+from routers.todos import get_db, get_current_user
 from fastapi.testclient import TestClient
 from fastapi import status
 
