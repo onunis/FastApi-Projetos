6aecc45c9103fac02dde349789924b7cc7b2a6bd
Guilherme <mitaogui@gmail.com>
2026-09-15 21:56:31 -0300

    feat: adicionando informacao no teste pra voltar uma lista vazia (o que temos)

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -35,3 +35,5 @@ client = TestClient(app)
 def test_read_all_authenticated():
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
+
