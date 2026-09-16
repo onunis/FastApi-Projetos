63e5425ca0c4eaa691f53ae027d24efdc9d7aaf6
Guilherme <mitaogui@gmail.com>
2026-09-15 21:49:24 -0300

    feat: adicionando teste pra testar se nossas funcoes overrides estao ok

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -30,3 +30,7 @@ app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
 client = TestClient(app)
+
+def test_read_all_authenticated():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
