22d404574ee90d8eb37abd6b777767d8577235dc
Guilherme <mitaogui@gmail.com>
2026-09-16 21:31:45 -0300

    feat: teste pra pegar todo nao existente

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -80,3 +80,10 @@ def test_read_one_authenticated(test_todo):
             'owner_id':1
         }
 
+
+def test_read_one_not_authenticated(test_todo):
+    response = client.get("/todo/999")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found"}
+
