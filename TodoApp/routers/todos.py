3620a6f285e9cb9c8216f5519faa712a487918a3
Guilherme <mitaogui@gmail.com>
2026-09-16 21:18:50 -0300

    fix: corrigindo endpoint que busca todo por id, onde nao tinha .first() pra pegar o primeiro

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -39,10 +39,10 @@ async def read_todo(user: user_dependency, db: db_dependency, todo_id: int = Pat
     if user is None:
             return HTTPException(status_code=401, detail="Authentication Failed")
     
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get("id"))
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get("id")).first()
     if todo_model is not None:
         return todo_model
-    raise HTTPException(status_code=404, detail="Id not found")
+    raise HTTPException(status_code=404, detail="Todo not found")
 
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
