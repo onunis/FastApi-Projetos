26f3ef56612235a136278e2264e6b7b4c99a5166
Guilherme <mitaogui@gmail.com>
2026-09-19 11:04:08 -0300

    So importanto utils pra poder usar das fixtures e funcoes

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,14 +1,10 @@
-from routers.todos import get_db, get_current_user
 from fastapi import status
-
-
+from routers.todos import get_db, get_current_user
+from .utils import *
 
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
-
-
-
 
 
 def test_read_all_authenticated(test_todo):
