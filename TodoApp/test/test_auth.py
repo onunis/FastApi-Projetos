88a36d5ada0f2084fadbe020f7fef4acd4a73316
Guilherme <mitaogui@gmail.com>
2026-09-21 20:44:23 -0300

    feat: criando testes pra usuarios autenticados e nao autenticados, senha errada e etc

--- TodoApp/test/test_auth.py
+++ TodoApp/test/test_auth.py
@@ -3,3 +3,32 @@ from routers.auth import get_db, authenticated_user
 
 app.dependency_overrides[get_db] = override_get_db
 
+
+def test_authenticate_user(test_user):
+
+    db = TestingSessionLocal()
+
+    authenticate_user = authenticated_user(
+        test_user.username,
+        "testpassword",
+        db
+    )
+
+    assert authenticate_user is not None
+    assert authenticate_user.username == test_user.username
+
+    non_existent_user = authenticated_user(
+        "WrongUserName",
+        "testpassword",
+        db
+    )
+
+    assert non_existent_user is False
+
+    user_with_wrong_password = authenticated_user(
+        test_user.username,
+        "wrongpassword",
+        db
+    )
+
+    assert user_with_wrong_password is False
