af3282f0f936700e47564795c81c38fed967574e
Guilherme <mitaogui@gmail.com>
2026-09-21 21:13:14 -0300

    criando teste pra validar token

--- TodoApp/test/test_auth.py
+++ TodoApp/test/test_auth.py
@@ -2,7 +2,12 @@ from jose import jwt
 from datetime import timedelta
 from .utils import *
 from routers.auth import (
-    get_db, authenticated_user, create_access_token, SECRET_KEY, ALGORITHM
+    get_db,
+    authenticated_user,
+    get_current_user,
+    create_access_token,
+    SECRET_KEY,
+    ALGORITHM
     )
 
 app.dependency_overrides[get_db] = override_get_db
@@ -62,3 +67,26 @@ def test_create_access_token():
     assert decoded_token["id"] == user_id
     assert decoded_token["role"] == role
 
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    encode = {
+        "sub": "testuser",
+        "id": 1,
+        "role": "admin"
+
+    }
+
+    token = jwt.encode(
+        encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    user = await get_current_user(token=token)
+
+    assert user ==  {
+        "username": "testuser",
+        "id": 1,
+        "user_role": "admin"
+    }
